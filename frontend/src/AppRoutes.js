53c38f4d4291ba246babc04d89b26ec7a6ca9614
Aryan apte <b20186@students.iitmandi.ac.in>
2023-09-06 03:53:47 +0530

    Added Search Bar, Tags, And Individual Food Pages

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import HomePage from './pages/Home/HomePage'
 import { Routes, Route } from 'react-router-dom'
+import FoodPage from './pages/Food/FoodPage'
 export default function AppRoutes() {
   return (
     <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/search/:searchTerm" element={<HomePage />} />
+        <Route path="/tag/:tag" element={<HomePage />} />
+        <Route path="/menu/:id" element={<FoodPage />} />
     </Routes>
   )
 }
--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useReducer } from 'react'
 import { getAll } from '../../services/foodService';
 import Thumbnails from '../../components/Thumbnails/Thumbnails';
-const initialState = {foods: []};
+import { search } from '../../services/foodService';
+import { useParams } from 'react-router-dom';
+import Search from '../../components/Search/Search';
+import Tags from '../../components/Tags/Tags';
+import { getAllTags, getAllByTag } from '../../services/foodService';
+
+const initialState = {foods: [], tags: []};
 
 const reducer = (state, action) => {
   switch(action.type){
     case 'FOODS_LOADED':
         return {...state, foods: action.payload}
+    case 'TAGS_LOADED':
+      return {...state, tags: action.payload}
     default:
         return state;
   }
@@ -15,15 +23,25 @@ const reducer = (state, action) => {
 export default function HomePage() {
 
   const [state, dispatch] = useReducer(reducer, initialState);
-  const {foods} = state;
-
+  const {foods, tags} = state;
+  const { searchTerm, tag } = useParams();
 
   useEffect(() =>{
-    getAll().then(foods => dispatch({type: 'FOODS_LOADED', payload: foods}));
-  }, []);
+    getAllTags().then(tags => dispatch({ type: 'TAGS_LOADED', payload: tags}));
+
+    const loadFoods = 
+    tag? getAllByTag(tag)
+    :searchTerm 
+    ? search(searchTerm) 
+    : getAll();
+
+    loadFoods.then(foods => dispatch({type: 'FOODS_LOADED', payload: foods}));
+  }, [searchTerm, tag]);
 
   return (
     <>
+      <Search />
+      <Tags tags={tags}/>
       <Thumbnails foods={foods}/>
     </>
   )

0495ad48bd8cd4b4913cc57cd167fe53d0391838
Aryan apte <b20186@students.iitmandi.ac.in>
2023-09-13 01:27:28 +0530

    changed the web link

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: ['https://cuisine-connect-pf9r.onrender.com/'],
+    origin: ['https://cuisine-connect-pf9r.onrender.com'],
 }))
 
 app.use('/api/foods', foodRouter);

283fba8b81c4ac8727b57cff5051edb4793302d5
Aryan apte <b20186@students.iitmandi.ac.in>
2023-09-13 01:13:42 +0530

    Added website link for deploying backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:3000'],
+    origin: ['https://cuisine-connect-pf9r.onrender.com/'],
 }))
 
 app.use('/api/foods', foodRouter);
